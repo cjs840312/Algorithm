bd0d3cdf6a664af9b34c94361836d6653097114a
Xwei <tsai.dennis12@gmail.com>
2016-06-07 11:36:10 +0800

    modify src/util/help_function.h

--- src/util/help_function.h
+++ src/util/help_function.h
@@ -5,7 +5,7 @@
 using namespace std;
 
 int myStrNCmp(const string&, const string&, unsigned);
-size_t myStrGetTok(const string&, string& , size_t=0,const char=' ');
+size_t myStrGetTok(const string&, string& , size_t=0, const char=' ');
 //int myStr2Tok(const string& , vector<string>& , int=0);
 bool myStr2Int(const string& , int& );
 
